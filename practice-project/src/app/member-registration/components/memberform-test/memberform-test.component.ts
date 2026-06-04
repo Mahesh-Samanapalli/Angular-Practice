a7ac50fe9b7d464a21d3ea18132e8981a0a41ae0
Mahesh Samanapalli <samanapalli.mahesh26@gmail.com>
2026-06-04 17:27:51 +0530

    api response object updated to render self

--- practice-project/src/app/member-registration/components/memberform-test/memberform-test.component.ts
+++ practice-project/src/app/member-registration/components/memberform-test/memberform-test.component.ts
@@ -13,7 +13,7 @@ import {
 })
 export class MemberformTestComponent implements OnInit {
   personalDetailsForm!: FormGroup;
-  apiResponse = [{ firstName: 'John', lastName: 'Doe' },{ firstName: 'Jane', lastName: 'Smith' }];
+  apiResponse = [{ firstName: 'John', lastName: 'Doe' }];
 
   constructor(private fb: FormBuilder) {}
 
